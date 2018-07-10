e318e015a9d952710a319318896f99e562bf4718
Tanel Hiob <tanelhiob@outlook.com>
2018-07-10 10:36:04 +0300

    Better example code

--- AzureMessageProcessing.Processes/Workers.cs
+++ AzureMessageProcessing.Processes/Workers.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Text;
@@ -7,33 +8,120 @@ using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.WindowsAzure.Storage.Queue;
+using Microsoft.WindowsAzure.Storage.Table;
+using Newtonsoft.Json;
 
 namespace AzureMessageProcessing.Processes
 {
+    public class Person : TableEntity
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string DataSetId { get; set; }
+    }
+
+    public class Message
+    {
+        public Guid ContentId { get; set; }
+        public string From { get; set; }
+        public DateTimeOffset Created { get; set; }
+    }
+
     public static class Workers
     {
         [FunctionName("Hello")]
-        public static async Task<HttpResponseMessage> Hello([HttpTrigger] HttpRequestMessage request, [Queue("onramp")] CloudQueue queue, TraceWriter traceWriter)
+        public static async Task<HttpResponseMessage> Hello(
+            [HttpTrigger] HttpRequestMessage request,
+            [Queue("onramp")] CloudQueue queue,
+            [Table("data")] CloudTable table,
+            TraceWriter traceWriter)
         {
-            traceWriter.Info("Hello, World!");
+            var dataSetId = Guid.NewGuid();
 
-            await queue.AddMessageAsync(new CloudQueueMessage("Hello, world!"));
+            var op = new TableBatchOperation
+            {
+                TableOperation.Insert(new Person { RowKey = Guid.NewGuid().ToString(), PartitionKey = string.Empty, FirstName = "Tanel", LastName = "Hiob", Age = 28, DataSetId = dataSetId.ToString() }),
+                TableOperation.Insert(new Person { RowKey = Guid.NewGuid().ToString(), PartitionKey = string.Empty, FirstName = "Liisi", LastName = "Mõtshärg", Age = 26, DataSetId = dataSetId.ToString() })
+            };
 
-            return new HttpResponseMessage(HttpStatusCode.OK) { Content = new StringContent("Hello, World!") };
+            await table.CreateIfNotExistsAsync();
+            await table.ExecuteBatchAsync(op);
+
+            var message = new Message
+            {
+                ContentId = dataSetId,
+                Created = DateTimeOffset.Now,
+                From = "Tanel",
+            };
+
+            await queue.AddMessageAsync(new CloudQueueMessage(JsonConvert.SerializeObject(message)));
+
+            return new HttpResponseMessage(HttpStatusCode.OK) { Content = new StringContent("Message added to the queue!") };
         }
 
         [FunctionName("OnRamp")]
-        public static async Task OnRampMessage([QueueTrigger("onramp")] string message, [Queue("consumption")] CloudQueue queue, TraceWriter log)
+        public static async Task OnRampMessage(
+            [QueueTrigger("onramp")] Message message,
+            [Queue("consumption")] CloudQueue consumptionQueue,
+            [Queue("dedicated")] CloudQueue dedicatedQueue,
+            TraceWriter traceWriter)
         {
-            // TODO: decide which type of client
-
-            await queue.AddMessageAsync(new CloudQueueMessage(message));
+            if (message.From == "Tanel")
+            {
+                await dedicatedQueue.AddMessageAsync(new CloudQueueMessage(JsonConvert.SerializeObject(message)));
+            }
+            else
+            {
+                await consumptionQueue.AddMessageAsync(new CloudQueueMessage(JsonConvert.SerializeObject(message)));
+            }
         }
 
-        [FunctionName("Processor")]
-        public static void Processor([QueueTrigger("consumption")] string message, TraceWriter log)
+        private static async Task Processor(Message message, CloudTable table, TraceWriter traceWriter)
         {
-            log.Info("Handled!");
+            var query = new TableQuery<Person>().Where(TableQuery.GenerateFilterCondition(nameof(Person.DataSetId), QueryComparisons.Equal, message.ContentId.ToString()));
+
+            List<Person> persons = new List<Person>();
+
+            TableContinuationToken token = null;
+            do
+            {
+                var segment = await table.ExecuteQuerySegmentedAsync(query, token);
+                token = segment.ContinuationToken;
+                persons.AddRange(segment.Results);
+            } while (token != null);
+
+            var highestAge = persons.Select(x => x.Age).DefaultIfEmpty(0).Max();
+            var personWithHighestAge = persons.FirstOrDefault(x => x.Age == highestAge);
+
+            if (personWithHighestAge != null)
+            {
+                traceWriter.Info($"{personWithHighestAge.FirstName} {personWithHighestAge.LastName} {personWithHighestAge.Age}");
+            }
+            else
+            {
+                traceWriter.Warning("no persons found at all");
+            }
+
+            traceWriter.Warning($"message processing time {DateTimeOffset.Now - message.Created}");
+        }
+
+        [FunctionName("ProcessorForConsumption")]
+        public static async Task ProcessorForConsumption(
+            [QueueTrigger("consumption")] Message message,
+            [Table("data")] CloudTable table,
+            TraceWriter traceWriter)
+        {
+            await Processor(message, table, traceWriter);
+        }
+
+        [FunctionName("ProcessorForDedicated")]
+        public static async Task ProcessorForDedicated(
+            [QueueTrigger("dedicated")] Message message,
+            [Table("data")] CloudTable table,
+            TraceWriter traceWriter)
+        {
+            await Processor(message, table, traceWriter);
         }
     }
 }
